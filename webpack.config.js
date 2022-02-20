f370298da972ffd33df3932c50ee4c2f74bad7c1
WhiteFyr <WhiteFyr1@gmail.com>
2022-02-19 23:19:10 -0500

    Adding webpack, ts, and package configs to repo

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    //entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'src'),
+        publicPath: 'http://localhost:8080/',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(tsx)$/,
+                exclude: /node_module/,
+                loader: 'ts-loader'
+            },
+            {
+                test: /\.(jpg|jpeg|gif|eot|otf|webp|mp4|webm|wav|mp3|m4a|aac|oga|ico|svg|woff|woff2|png|ttf)$/,
+                use: [{
+                    loader:'file-loader'
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html')
+        })
+    ],
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    }
+}
